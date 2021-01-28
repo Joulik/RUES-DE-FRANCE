b4e07300e352f3a792aa0dfe74c05d2fc740af7f
Joulik <50136475+Joulik@users.noreply.github.com>
2021-01-28 23:18:23 +0100

    Constructed SQL DB (2 tables) with pandas

--- rues_france/admin.py
+++ rues_france/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from rues_france.models import StreetQuery 
+from rues_france.models import StreetQuery,FranceRues,CommuneDepartementRegion 
 
 # Register your models here.
 admin.site.register(StreetQuery)
+admin.site.register(FranceRues)
+admin.site.register(CommuneDepartementRegion)
