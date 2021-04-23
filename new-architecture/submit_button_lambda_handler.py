bfb1a186917422c0aac066b54013b056db7f29bd
Joulik <leroy.prof.SPC@gmail.com>
2021-04-23 14:55:48 +0200

    Added .py script to store codes dict and SQL queries

--- new-architecture/submit_button_lambda_handler.py
+++ new-architecture/submit_button_lambda_handler.py
@@ -2,10 +2,23 @@ import boto3
 import json
 import logging
 import time
+from constants import fr_region_codes,query_regions
+from constants import fr_department_codes
+
+
+def init_results_dict(fr_codes):
+    """
+    This function initializes the results dictionnary
+    """
+    
+    res_dict = {}
+    for k,v in fr_codes.items():
+        res_dict[v] = 0
+    
+    return res_dict
 
 
 def handler(event, context):
-    
     """
     This function is called by AWS every time the 
     Submit button Lambda is triggered
@@ -13,54 +26,18 @@ def handler(event, context):
     
     # define logging default level
     log_level = logging.INFO
-
     logging.basicConfig(level=log_level)
-    
-    # define equivalence btw actual region names and map region codes
-    fr_region_codes = {
-    "Auvergne-Rhône-Alpes":"FR-X7",    
-    "Bourgogne-Franche-Comté":"FR-X1",
-    "Bretagne":"FR-E",
-    "Centre-Val de Loire":"FR-F",
-    "Corse":"FR-H",
-    "Grand Est":"FR-X4",
-    "Guadeloupe":"FR-GP",
-    "Guyane":"FR-GF",
-    "Hauts-de-France":"FR-X6",
-    "La Réunion":"FR-RE",
-    "Martinique":"FR-MQ",
-    "Mayotte":"FR-YT",
-    "Normandie":"FR-X3",
-    "Nouvelle-Aquitaine":"FR-X2",
-    "Occitanie":"FR-X5",
-    "Pays de la Loire":"FR-R",
-    "Provence-Alpes-Côte d'Azur":"FR-U",
-    "Île-de-France":"FR-J"
-    }    
-    
-    # initialize results dictionary
-    final_dict = {}
-    for k,v in fr_region_codes.items():
-        final_dict[v] = 0
-    
+
     # parse the stringified json sent from javascript
     event = json.loads(event['body'])
 
+    # extract street name for query
     street_name = event['street_name']
     
     # setup and perform query
     client = boto3.client('athena')
     
-    query = '''SELECT COUNT(aa.nom_comm),aa.nom_region 
-            FROM
-            (SELECT DISTINCT(fr.nom_comm),fr.voie,fr.code_post,cdr.nom_region,cdr.latitude,cdr.longitude
-            FROM default.france_rues AS fr
-            JOIN default.communes_departements_regions AS cdr
-            ON fr.nom_comm=cdr.nom_commune
-            AND fr.code_post=cdr.code_postal
-            WHERE fr.voie LIKE '%{}') AS aa
-            GROUP BY aa.nom_region
-            ORDER BY aa.nom_region'''.format(street_name)
+    query = query_regions.format(street_name)
     
     DATABASE = 'default'
     output='s3://ruesdefrance-query-results/'
@@ -75,7 +52,7 @@ def handler(event, context):
         }
     )
     
-    # observe result
+    # get query execution ID
     queryId = queryStart['QueryExecutionId']
     
     # prepare results if request succeeds
@@ -92,7 +69,8 @@ def handler(event, context):
         elif status == 'FAILED':
             print('FAILED')
         time.sleep(1)
-        
+    
+    # extract column names and content of rows    
     columns = rows[0]
     rows = rows[1:]
     
@@ -100,28 +78,33 @@ def handler(event, context):
     for column in columns:
         columns_list.append(column['VarCharValue'])
     
-    # construct result dict with region names 
-    regions_dict = {}
+    # construct result dict with region or department names 
+    results_dict = {}
     for row in rows:
         k = row[1]['VarCharValue']
         v = row[0]['VarCharValue']
-        regions_dict[k] = int(v)
+        results_dict[k] = int(v)
+    
+    # initialize results dictionary
+    final_result_dict = init_results_dict(fr_region_codes)
     
     # map region names with region codes to construct result dict
-    for k1,v1 in regions_dict.items():
+    for k1,v1 in results_dict.items():
         for k2,v2 in fr_region_codes.items():
             if k1==k2:
-                final_dict[v2]=v1
+                final_result_dict[v2]=v1
     
     # total number of streets for request 
     total_street_nbr = 0
-    for k,v in final_dict.items():
+    for k,v in final_result_dict.items():
             total_street_nbr += v
             
     print("Total: {}".format(total_street_nbr))
     
     # dict to send back to JS
-    subreports = {"nbr_streets_per_region":final_dict,"total_street_nbr":"<p>Total {}</p>".format(total_street_nbr)}
+    subreports = {"nbr_streets_per_region":final_result_dict, \
+                "total_street_nbr":"<p>Il y a {} résultats pour {}.</p>" \
+                .format(total_street_nbr,street_name)}
 
     return {
         'statusCode': 200,
