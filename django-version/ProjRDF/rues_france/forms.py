54e8b6371fc79ef6ec27fe3aa91f2270c4a84ac6
Joulik <leroy.prof.SPC@gmail.com>
2021-04-30 21:50:02 +0200

    Removed django version from remote repo

--- django-version/ProjRDF/rues_france/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from rues_france.models import StreetQuery
-
-class StreetQueryForm(forms.ModelForm): #create class for new street query
-    #place cross validation here in case needed
-
-    class Meta(): #create inline class
-        model = StreetQuery
-        fields = '__all__' #use all field attributes
